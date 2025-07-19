8d6603cf97d7fb875bf87cc88a56549b77ccdf52
alimarashian <45737450+alimarashian@users.noreply.github.com>
2025-07-19 22:46:23 +0000

    Deploying to gh-pages from @ alimarashian/alimarashian.github.io@384f7d2e8784a542ac9697b982d632ed990a8ba3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -68,6 +68,13 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/ali-marashian", "_blank");
+        },
+      },{
         id: 'social-rss',
         title: 'RSS Feed',
         section: 'Socials',
@@ -79,14 +86,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=jEHAaOYAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
